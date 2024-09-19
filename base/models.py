00c658fa186ecf3199922ac3697764b451b7e6fd
Alejandro Pina <145475022+alexjambro@users.noreply.github.com>
2024-09-18 22:59:32 -0400

    3:27 crud messages

--- base/models.py
+++ base/models.py
@@ -12,7 +12,7 @@ class Room(models.Model):
     topic= models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank =True)
-    #participants =
+    participants = models.ManyToManyField(User, related_name='particpants',blank=True)
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True) #only takes timestamp first save/create
     class Meta:
